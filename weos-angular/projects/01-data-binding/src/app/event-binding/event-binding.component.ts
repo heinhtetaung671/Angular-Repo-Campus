a1796433587d0e2440b4151690cf4547ece1b7c6
heinhtetaung671 <hhein8358@gmail.com>
2024-03-02 11:20:24 +0630

    manual twoway binding

--- weos-angular/projects/01-data-binding/src/app/event-binding/event-binding.component.ts
+++ weos-angular/projects/01-data-binding/src/app/event-binding/event-binding.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-event-binding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './event-binding.component.html'
 })
 export class EventBindingComponent {
+
+  num: number = Number.parseInt((Math.random() * 100).toString())
+
+  myStyle = {
+    border: '1px solid red',
+    padding: '1.2rem',
+    borderLeft: '5px solid red'
+  }
+
+  myClassObj = {}
 
 }
--- weos-angular/projects/01-data-binding/src/app/two-way-binding/two-way-binding.component.ts
+++ weos-angular/projects/01-data-binding/src/app/two-way-binding/two-way-binding.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { User } from '../model/data/user';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-two-way-binding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './two-way-binding.component.html'
 })
 export class TwoWayBindingComponent {
+
+  user: User = {
+    name: 'Ro',
+    phone: '+9588888888',
+    gender: 'Male'
+  }
+
+  genderArray: ['Male', 'Female', 'Other'] = ['Male', 'Female', 'Other']
+
+  saveData(name: string, phone: string, gender: string) {
+    this.user.name = name;
+    this.user.phone = phone;
+    this.user.gender = gender as 'Male' | 'Female' | 'Other';
+  }
+
+  convertGender(gender: string): 'Male' | 'Female' | 'Other' {
+    return gender as 'Male' | 'Female' | 'Other'
+  }
 
 }
